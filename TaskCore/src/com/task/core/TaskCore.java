8970f4bd88772243d3bb1f63f8a07b71ffcc7370
Bin Lu <seanlu2001@gmail.com>
2013-06-23 22:07:08 +1000

    add core and main test

---
+++ TaskCore/src/com/task/core/TaskCore.java
@@ -0,0 +1,5 @@
+package com.task.core;
+
+public class TaskCore {
+
+}
---
+++ TaskCore/src/com/task/core/TestCore.java
@@ -0,0 +1,13 @@
+package com.task.core;
+
+public class TestCore {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
